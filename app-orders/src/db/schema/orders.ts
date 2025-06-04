ac459a106cb7cf0fbbcfae9f33464b215b19845a
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-06-04 11:48:16 -0300

    chore(core): create customers table schema & migrations

--- app-orders/src/db/schema/orders.ts
+++ app-orders/src/db/schema/orders.ts
@@ -1,5 +1,7 @@
 import { pgTable, pgEnum, text, integer, timestamp } from 'drizzle-orm/pg-core'
 
+import { customers } from './customers.ts'
+
 export const orderStatusEnum = pgEnum('order_status', [
   'pending',
   'paid',
@@ -8,7 +10,9 @@ export const orderStatusEnum = pgEnum('order_status', [
 
 export const orders = pgTable('orders', {
   id: text().primaryKey(),
-  customerId: text().notNull(),
+  customerId: text()
+    .notNull()
+    .references(() => customers.id),
   amount: integer().notNull(),
   status: orderStatusEnum().notNull().default('pending'),
   createdAt: timestamp().notNull().defaultNow(),
